30962c8d68a1ddc998fbd736890f4e57d27456bd
Grzegorz Świrski <grzegorz@swirski.name>
2024-06-04 21:31:25 +0200

    remove unused ark-serialize

--- rust/src/error.rs
+++ rust/src/error.rs
@@ -1,11 +1,8 @@
 use ark_ec::hashing::HashToCurveError;
-use ark_serialize::SerializationError;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("couldn't serialize point or field element: {0}")]
-    SerializationError(SerializationError),
     #[error("couldn't map hash to curve: {0}")]
     HashToCurveError(HashToCurveError),
     #[error("couldn't verify proof")]
@@ -15,12 +12,6 @@ pub enum Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
-
-impl From<SerializationError> for Error {
-    fn from(value: SerializationError) -> Self {
-        Self::SerializationError(value)
-    }
-}
 
 impl From<HashToCurveError> for Error {
     fn from(value: HashToCurveError) -> Self {
